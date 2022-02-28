c5c6567c60e4dc183d9973711efe26c6b729b111
bbeebe1 <98788652+bbeebe1@users.noreply.github.com>
2022-02-27 23:03:34 -0500

    Update ChessGame.py

--- ChessGame.py
+++ ChessGame.py
@@ -1,5 +1,6 @@
 import random
 from turnManager import TurnManager
+from ThreeCorp import corp
 
 class Piece:
     def __init__(self, x: int, y: int, name: str, white: bool, type: str):
@@ -9,6 +10,7 @@ class Piece:
         self.__name = name
         self.__white = white
         self.__type = type
+        self.corp = None
 
     def set_killed(self):
         self.killed = True
@@ -21,6 +23,15 @@ class Piece:
 
     def get_type(self):
         return self.__type
+    
+    def set_corp(self, corp):
+        self.corp = corp
+
+    def has_moved(self):
+        return self.corp.hasCommanded()
+
+    def set_moved(self):
+        self.corp.command()
 
 class Spot:
     def __init__(self, x: int, y: int, piece: Piece):
@@ -69,9 +80,71 @@ class Game:
                    Piece(3, 7, 'wQ', white=True, type="Queen"), Piece(3, 0, 'bQ', white=False, type="Queen"),
                    Piece(4, 7, 'wKg', white=True, type="King"), Piece(4, 0, 'bKg', white=False, type="King")])
 
+        #creating the three corps for each color and adding the pieces to them
+        print('\n')
+        self.corpW1 = corp('corpW1', pieces[16])
+        pieces[16].corp = self.corpW1
+        self.corpW1.addToCorp(pieces[0])
+        self.corpW1.addToCorp(pieces[1])
+        self.corpW1.addToCorp(pieces[2])
+        self.corpW1.addToCorp(pieces[24])
+        #corpW1.printCorp()
+
+        self.corpW2 = corp('corpW2', pieces[30])
+        pieces[30].corp = self.corpW2
+        self.corpW2.addToCorp(pieces[3])
+        self.corpW2.addToCorp(pieces[4])
+        self.corpW2.addToCorp(pieces[20])
+        self.corpW2.addToCorp(pieces[21])
+        self.corpW2.addToCorp(pieces[28])
+        #corpW2.printCorp()
+
+        self.corpW3 = corp('corpW3', pieces[17])
+        pieces[17].corp = self.corpW3
+        self.corpW3.addToCorp(pieces[5])
+        self.corpW3.addToCorp(pieces[6])
+        self.corpW3.addToCorp(pieces[7])
+        self.corpW3.addToCorp(pieces[25])
+        #corpW3.printCorp()
+
+        self.corpB1 = corp('corpB1', pieces[18])
+        pieces[18].corp = self.corpB1
+        self.corpB1.addToCorp(pieces[8])
+        self.corpB1.addToCorp(pieces[9])
+        self.corpB1.addToCorp(pieces[10])
+        self.corpB1.addToCorp(pieces[26])
+        #corpB1.printCorp()
+
+
+        self.corpB2 = corp('corpB2', pieces[31])
+        pieces[31].corp = self.corpB2
+        self.corpB2.addToCorp(pieces[11])
+        self.corpB2.addToCorp(pieces[12])
+        self.corpB2.addToCorp(pieces[22])
+        self.corpB2.addToCorp(pieces[23])
+        self.corpB2.addToCorp(pieces[29])
+        #corpB2.printCorp()
+
+
+        self.corpB3 = corp('corpB3', pieces[19])
+        pieces[19].corp = self.corpB3
+        self.corpB3.addToCorp(pieces[13])
+        self.corpB3.addToCorp(pieces[14])
+        self.corpB3.addToCorp(pieces[15])
+        self.corpB3.addToCorp(pieces[27])
+        #corpB3.printCorp()
+        
         #assign pieces to board
         for p in pieces:
             self.__board[p.y_loc][p.x_loc].set_piece(p)
+            
+    def resetTurn(self):   #Used to reset corp command count for each corp
+        self.corpW1.resetCommand()
+        self.corpW2.resetCommand()
+        self.corpW3.resetCommand()
+        self.corpB1.resetCommand()
+        self.corpB2.resetCommand()
+        self.corpB3.resetCommand()
 
     #returns array of tuples containing co-ords of possible move spots
     def get_possible_moves_for_piece_at(self, *, x:int, y:int):
@@ -138,6 +211,12 @@ class Game:
             return False
         if from_x==to_x and from_y==to_y:
             return
+        
+        useOne = False
+        if abs(from_spot.x_loc - to_spot.x_loc) <= 1 and abs(from_spot.y_loc - to_spot.y_loc) <= 1 and (from_spot.piece.get_type() == 'Bishop' or from_spot.piece.get_type()== 'King') and not from_spot.piece.corp.commanderMoved():
+            print('test')
+            useOne = True
+        
         #move
         self.__move_message = f"{from_spot.piece.get_name()} to ({str(to_x)}, {str(to_y)}): "
         if not self.__is_valid_move(from_x, from_y, to_x, to_y):
@@ -157,31 +236,67 @@ class Game:
                     self.__move_message += "Attempting capture... "
                     if self.__is_attack_successful(from_spot.piece, to_spot.piece):
                         self.__move_message += "Success! Captured piece! "
+                        if to_spot.piece.get_type() == 'King':
+                            print('you win')
+                            return
+                        elif to_spot.piece.get_type() == 'Bishop':
+                            if to_spot.piece.is_white():
+                                to_spot.piece.corp.captured(self.corpW2)
+                            else:
+                                to_spot.piece.corp.captured(self.corpB2)
+                        else:
+                            to_spot.piece.corp.removeFromCorp(to_spot.piece)
                         to_spot.piece.set_killed()
                         piece_color = "white" if from_spot.piece.is_white else "black"
                         self.__captured_by[piece_color].append(to_spot.piece)
                     else:
                         self.__move_message += "Attack failed! Move unsuccessful! "
                         # we still technicly used an action so we must progress turnManager
+                        from_spot.piece.set_moved()
+                        temp = self.tracker.current_player
                         self.tracker.use_action()
+                        if temp is not self.tracker.current_player:
+                            self.resetTurn()
                         print(self.__move_message)
                         return False
             #empty spot
             else:
                 self.__move_message += "Targeted spot is empty... "
+            if useOne == True:
+                print('using commander single space move')
+                from_spot.piece.corp.movedOne()
+            else:
+                print('using command authority')
+                from_spot.piece.set_moved()
             self.__move_message += "Moving to spot. "
             print(self.__move_message)
             to_spot.piece = from_spot.piece
             from_spot.piece = None
             to_spot.piece.x_loc = to_x
             to_spot.piece.y_loc = to_y
+            temp = self.tracker.current_player
             self.tracker.use_action()
+            if temp is not self.tracker.current_player:
+                self.resetTurn()
         print("~~~~~")
         self.print_board()
         return True
     
     def __is_valid_move(self, from_x: int, from_y: int, to_x: int, to_y: int):
         self.__move_list = []
+        
+        #Checks to see if this piece's corp has already used its comman
+        piece=self.__board[from_y][from_x].piece
+        if abs(from_x - to_x) <= 1 and abs(from_y - to_y) <= 1 and (piece.get_type() == 'Bishop' or piece.get_type()== 'King'):
+            #print(from_spot.x_loc, from_spot.y_loc)
+            if piece.corp.commanderMoved():
+                if piece.has_moved():
+                    print('has used all moves')
+                    return False
+                #print('using command authority')
+        elif piece.has_moved() == True:
+            print('This corp has already used its authority')
+            return False
         
         #check for bounds
         if to_x>7 or to_y>7 or to_x<0 or to_y<0:
